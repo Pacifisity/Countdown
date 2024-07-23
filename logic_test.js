b1149a6122d3c8409e75595c7ae56b69f3c7422e
Skyler Church <Pacifisity@gmail.com>
2024-07-23 16:04:10 -0400

    pretty

--- logic_test.js
+++ logic_test.js
@@ -1,33 +1,41 @@
-document.getElementById('numForm').addEventListener('submit', function (event) {
-	event.preventDefault();
-	let age = document.getElementById('age').value;
+document
+	.getElementById('biologyForm')
+	.addEventListener('submit', function (event) {
+		event.preventDefault();
+		let birthday = document.getElementById('birthday').value;
 
-	let lifespan = document.getElementById('lifespan').value;
-	if (age > lifespan) {
-		lifespan = age; // Dancing with death be like
-	}
+		let sex = document.getElementsByName('sex');
 
-	let num = Math.ceil(Math.sqrt(lifespan));
+		if (sex[0].checked) {
+			lifespan = 73;
+		} else {
+			lifespan = 80;
+		}
 
-	generateTable(num, age, lifespan);
-});
+		timeSpent = timeSpentCalculator(birthday, dateGenerator());
 
-function generateTable(num, age, lifespan) {
+		age = timeSpent[0];
+
+		let rows = Math.ceil((lifespan + 1) / 10);
+
+		generateTable(rows, age, lifespan);
+	});
+
+function generateTable(rows, age, lifespan) {
 	let tableContainer = document.getElementById('tableContainer');
 	let tableHTML = '<table>';
 
-	for (let i = 0; i < num; i++) {
+	for (let i = 0; i < rows; i++) {
 		tableHTML += '<tr>';
 		for (let j = 0; j < 10; j++) {
-			// Ensure exactly 10 columns
-			let cells = i * 10 + j; // Calculate the current cell number
+			let cells = i * 10 + j;
 
 			if (age > cells) {
-				tableHTML += `<td style="background-color: black; color: white">${cells}</td>`;
+				tableHTML += `<td class="past">${cells}</td>`;
 			} else if (age == cells) {
-				tableHTML += `<td style="background-color: grey">${cells}</td>`;
+				tableHTML += `<td class="current">${cells}</td>`;
 			} else if (lifespan >= cells) {
-				tableHTML += `<td>${cells}</td>`;
+				tableHTML += `<td class="future">${cells}</td>`;
 			}
 		}
 		tableHTML += '</tr>';
@@ -35,4 +43,49 @@ function generateTable(num, age, lifespan) {
 
 	tableHTML += '</table>';
 	tableContainer.innerHTML = tableHTML;
+}
+
+function dateGenerator() {
+	let currentDate = new Date();
+
+	let day = currentDate.getDate();
+	let month = currentDate.getMonth() + 1;
+	let year = currentDate.getFullYear();
+
+	// Pad the month and day with leading zeros if necessary
+	if (month < 10) {
+		month = '0' + month;
+	}
+
+	if (day < 10) {
+		day = '0' + day;
+	}
+
+	// Create the formatted date string in yyyy-mm-dd format to work with the birthday collection form
+	let formattedDate = year + '-' + month + '-' + day;
+
+	return formattedDate;
+}
+
+function timeSpentCalculator(birthday, current) {
+	birthDate = birthday.split('-');
+	currentDate = current.split('-');
+
+	yearsSpent = currentDate[0] - birthDate[0];
+
+	monthsSpent = currentDate[1] - birthDate[1];
+	if (monthsSpent < 0) {
+		monthsSpent = monthsSpent + 12;
+		yearsSpent = yearsSpent - 1;
+	}
+
+	daysSpent = currentDate[2] - birthDate[2];
+	if (daysSpent < 0) {
+		daysSpent = daysSpent + 30;
+		monthsSpent = monthsSpent - 1;
+	}
+
+	timeSpent = [yearsSpent, monthsSpent, daysSpent];
+
+	return timeSpent;
 }
